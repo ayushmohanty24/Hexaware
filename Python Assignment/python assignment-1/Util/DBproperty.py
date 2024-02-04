132e568c60ea9c2f35e65a96264fefee9c639864
Ayush Mohanty <63730038+ayushmohanty24@users.noreply.github.com>
2024-02-04 17:51:38 +0530

    Add files via upload

---
+++ Python Assignment/python assignment-1/Util/DBproperty.py
@@ -0,0 +1,7 @@
+class PropertyUtil:
+    def getPropertyString():
+        host = 'localhost'
+        username = 'root'
+        password = 'root'
+        database = 'techshop'
+        return host, username, password, database
